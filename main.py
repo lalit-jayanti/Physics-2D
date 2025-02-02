fafd62514de43bd7685a60c73119ce3849ad47fe
Lalit <jayanti.lalit@gmail.com>
2025-02-03 04:05:11 +0530

    refactor collision code

--- main.py
+++ main.py
@@ -67,6 +67,36 @@ class Environment:
 
         return ((m1-m2)*v1 + 2*m2*v2)/(m1+m2)
 
+    def handle_circle_circle(self, obj1, obj2):
+
+        rad_pos = (np.linalg.norm(obj2.pos-obj1.pos))
+
+        diff = obj1.radius + obj2.radius - rad_pos
+
+        if diff < 0:
+            return
+
+        center_line = (obj2.pos-obj1.pos)/rad_pos
+
+        v1 = np.dot(obj1.vel, center_line)*center_line
+        v2 = np.dot(obj2.vel, center_line)*center_line
+
+        obj1.pos -= (
+            diff*center_line * (obj2.mass)/(obj1.mass + obj2.mass)
+        )
+        obj2.pos += (
+            diff*center_line * (obj1.mass)/(obj1.mass + obj2.mass)
+        )
+
+        if (np.dot((v2-v1), center_line) > 0):
+            return
+
+        obj1.vel -= v1
+        obj2.vel -= v2
+
+        obj1.vel += self.collision_vel(obj1.mass, obj2.mass, v1, v2)
+        obj2.vel += self.collision_vel(obj2.mass, obj1.mass, v2, v1)
+
     def brute_handle_collisions(self):
 
         for obj in self.objects:
@@ -79,34 +109,7 @@ class Environment:
             obj1 = self.objects[i]
             for j in range(i+1, len(self.objects)):
                 obj2 = self.objects[j]
-
-                rad_pos = (np.linalg.norm(obj2.pos-obj1.pos))
-
-                diff = obj1.radius + obj2.radius - rad_pos
-
-                if diff < 0:
-                    continue
-
-                center_line = (obj2.pos-obj1.pos)/rad_pos
-
-                v1 = np.dot(obj1.vel, center_line)*center_line
-                v2 = np.dot(obj2.vel, center_line)*center_line
-
-                obj1.pos -= (
-                    diff*center_line * (obj2.mass)/(obj1.mass + obj2.mass)
-                )
-                obj2.pos += (
-                    diff*center_line * (obj1.mass)/(obj1.mass + obj2.mass)
-                )
-
-                if (np.dot((v2-v1), center_line) > 0):
-                    continue
-
-                obj1.vel -= v1
-                obj2.vel -= v2
-
-                obj1.vel += self.collision_vel(obj1.mass, obj2.mass, v1, v2)
-                obj2.vel += self.collision_vel(obj2.mass, obj1.mass, v2, v1)
+                self.handle_circle_circle(obj1, obj2)
 
     def quad_tree_collisions(self):
 
@@ -132,33 +135,7 @@ class Environment:
                 if obj2 is obj1:
                     continue
 
-                rad_pos = (np.linalg.norm(obj2.pos-obj1.pos))
-
-                diff = obj1.radius + obj2.radius - rad_pos
-
-                if diff < 0:
-                    continue
-
-                center_line = (obj2.pos-obj1.pos)/rad_pos
-
-                v1 = np.dot(obj1.vel, center_line)*center_line
-                v2 = np.dot(obj2.vel, center_line)*center_line
-
-                obj1.pos -= (
-                    diff*center_line * (obj2.mass)/(obj1.mass + obj2.mass)
-                )
-                obj2.pos += (
-                    diff*center_line * (obj1.mass)/(obj1.mass + obj2.mass)
-                )
-
-                if (np.dot((v2-v1), center_line) > 0):
-                    continue
-
-                obj1.vel -= v1
-                obj2.vel -= v2
-
-                obj1.vel += self.collision_vel(obj1.mass, obj2.mass, v1, v2)
-                obj2.vel += self.collision_vel(obj2.mass, obj1.mass, v2, v1)
+                self.handle_circle_circle(obj1, obj2)
 
     def handle_collisions(self):
         self.quad_tree_collisions()
